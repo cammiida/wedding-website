2932d24332cf86193aad33006908d09ffd8105cb
Camilla <camillamdalan@gmail.com>
2023-04-10 13:40:36 +0200

    fixes login not visible after refactoring

--- app/routes/__layout.login.tsx
+++ app/routes/__layout.login.tsx
@@ -16,21 +16,23 @@ export async function loader({ request }: LoaderArgs) {
 
 const Login = () => {
   return (
-    <div className="flex max-w-xl flex-col items-center rounded-md bg-grey-transparent p-8">
-      <h1 className="text-center font-roboto text-3xl font-thin">
-        OH NO! IT LOOKS LIKE YOU'RE NOT LOGGED IN 😢
-      </h1>
-      <p className="max-w-md p-4 text-center font-thin">
-        Enter the provided password you have received in order to gain access.
-      </p>
-      <Form method="post">
-        <input
-          name="passphrase"
-          placeholder="Password to enter site here"
-          type="password"
-          className="h-10 w-96 rounded-md border-2 border-gray-600 p-3 text-black"
-        />
-      </Form>
+    <div className="flex h-screen items-center justify-center">
+      <div className="z-10 flex max-w-xl flex-col items-center rounded-md bg-grey-transparent  p-8">
+        <h1 className="text-center font-roboto text-3xl font-thin">
+          OH NO! IT LOOKS LIKE YOU'RE NOT LOGGED IN 😢
+        </h1>
+        <p className="max-w-md p-4 text-center font-thin">
+          Enter the provided password you have received in order to gain access.
+        </p>
+        <Form method="post">
+          <input
+            name="passphrase"
+            placeholder="Password to enter site here"
+            type="password"
+            className="h-10 w-96 rounded-md border-2 border-gray-600 p-3 text-black"
+          />
+        </Form>
+      </div>
     </div>
   );
 };
