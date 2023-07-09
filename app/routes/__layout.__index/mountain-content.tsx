48f240ee0ca4ecdd363143f4c3a21de4e5434f7c
Camilla <camillamdalan@gmail.com>
2023-07-09 17:15:59 +0200

    makes image text grid on landing page mobile friendly

--- app/routes/__layout.__index/mountain-content.tsx
+++ app/routes/__layout.__index/mountain-content.tsx
@@ -1,44 +1,59 @@
 import { Link } from "@remix-run/react";
-import React from "react";
 
 const MountainContent = () => {
   return (
     <div className="grid grid-rows-3 gap-12 py-12">
-      <div className="flex justify-center gap-24">
-        <Link to="/story" className="group w-1/4">
-          <h3 className="font-roboto text-2xl font-thin text-orange group-hover:subpixel-antialiased	">
-            HOW C&T MET
-          </h3>
-          <p className="pt-4 font-thin">
-            Prepare yourself for an exciting weekend.
-          </p>
-        </Link>
-        <img src="/login-bg.jpg" className="w-1/4 rounded-md" alt="" />
-      </div>
-      <div className="flex flex-row-reverse justify-center gap-24">
-        <Link to="/travel" className="group w-1/4">
-          <h3 className="font-roboto text-2xl font-thin text-orange group-hover:subpixel-antialiased">
-            HOW TO GET THERE
-          </h3>
-          <p className="pt-4 font-thin">
-            The wedding will take place at the Bygdin Mountain Hotel in the
+      <Element
+        title="HOW C&T MET"
+        content="Prepare yourself for an exciting weekend."
+        link="/story"
+        img="/login-bg.jpg"
+      />
+      <Element
+        title="HOW TO GET THERE"
+        content="The wedding will take place at the Bygdin Mountain Hotel in the
             majestic Hardangervidda, Norway. It is a four-hour bus ride from
-            Oslo, where Norway's main airport (Gardemoen) is located.
-          </p>
-        </Link>
-        <img src="/login-bg.jpg" className="w-1/4 rounded-md" alt="" />
-      </div>
-      <div className="flex justify-center gap-24">
-        <Link to="/sleep" className="group w-1/4">
-          <h3 className="font-roboto text-2xl font-thin text-orange group-hover:subpixel-antialiased">
-            WHERE WE CAN SLEEP
-          </h3>
-          <p className="pt-4 font-thin">
-            Prepare yourself for an exciting weekend.
-          </p>
-        </Link>
-        <img src="/login-bg.jpg" className="w-1/4 rounded-md" alt="" />
-      </div>
+            Oslo, where Norway's main airport (Gardemoen) is located."
+        link="/travel"
+        img="/login-bg.jpg"
+        reverse
+      />
+      <Element
+        title=" WHERE WE CAN SLEEP"
+        content=" Prepare yourself for an exciting weekend."
+        link="/sleep"
+        img="/login-bg.jpg"
+      />
+    </div>
+  );
+};
+
+const Element = ({
+  title,
+  content,
+  link,
+  img,
+  reverse = false,
+}: {
+  title: string;
+  content: string;
+  link: `/${string}`;
+  img: `/${string}.jpg`;
+  reverse?: boolean;
+}) => {
+  return (
+    <div
+      className={`flex flex-col items-center justify-center gap-24 lg:items-start ${
+        reverse ? "lg:flex-row-reverse" : "lg:flex-row"
+      }`}
+    >
+      <Link to={link} className="group w-2/3 lg:w-1/4">
+        <h3 className="font-roboto text-2xl font-thin text-orange group-hover:subpixel-antialiased	">
+          {title.toLocaleUpperCase()}
+        </h3>
+        <p className="pt-4 font-thin">{content}</p>
+      </Link>
+      <img src={img} className="w-2/3 rounded-md lg:w-1/4" alt="" />
     </div>
   );
 };
