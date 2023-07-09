701d9d3c5c28aa91c43786ad878fd4bd8fe59a7d
Camilla <camillamdalan@gmail.com>
2023-07-09 22:46:32 +0200

    start of real form with validation

--- app/routes/__layout.rsvp.tsx
+++ app/routes/__layout.rsvp.tsx
@@ -1,8 +1,42 @@
+import type { ActionArgs } from "@remix-run/node";
 import { json, type LoaderArgs } from "@remix-run/node";
-import { Form, useLoaderData, useSearchParams } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useSearchParams,
+} from "@remix-run/react";
+import { withZod } from "@remix-validated-form/with-zod";
+import { useState } from "react";
+import { z } from "zod";
 import type { Guest } from "~/guest-list/schema";
 import { useRootData } from "~/root";
 import { authenticator } from "~/services/authenticator.server";
+
+const rsvpSchema = z.union([
+  z.object({
+    name: z.string(),
+    isGoing: z.undefined().transform(() => false),
+  }),
+  z
+    .object({
+      name: z.string(),
+      isGoing: z.literal("on").transform(() => true),
+      address: z.string().nonempty(),
+      dietaryRestrictions: z.string().nullish(),
+      noGift: z.literal("on").transform(() => true),
+    })
+    .and(
+      z.union([
+        z.object({ plusOne: z.undefined().transform(() => false) }),
+        z.object({
+          plusOne: z.literal("on").transform(() => true),
+          plusOneName: z.string().min(3),
+          plusOneEmail: z.string().email(),
+        }),
+      ])
+    ),
+]);
 
 export async function loader({ request }: LoaderArgs) {
   authenticator.isAuthenticated(request, {
@@ -13,6 +47,14 @@ export async function loader({ request }: LoaderArgs) {
   const email = params.get("email");
 
   return json({ email });
+}
+
+export async function action({ request }: ActionArgs) {
+  const validatedFormValues = await withZod(rsvpSchema).validate(
+    await request.formData()
+  );
+
+  return json({ validationErrors: validatedFormValues.error });
 }
 
 const RSVP = () => {
@@ -68,10 +110,102 @@ const SearchGuestForm = () => {
 };
 
 const RsvpForm = ({ guest }: { guest: Guest }) => {
+  const data = useActionData<typeof action>();
+  const fieldErrors = data?.validationErrors?.fieldErrors;
+
+  const [isGoing, setIsGoing] = useState<boolean>(false);
+  const [bringingPlusOne, setBringingPlusOne] = useState<boolean>(false);
+
   return (
-    <Form method="post">
-      <input readOnly name="name" value={guest.name} />
-      <button type="submit">Submit</button>
+    <Form method="post" className="flex flex-col gap-3">
+      <h3 className=" font-bold">Responding for: {guest.name}</h3>
+      <input
+        readOnly
+        hidden
+        name="name"
+        value={guest.name}
+        className="w-full p-1 text-black placeholder:text-light-grey read-only:bg-light-grey"
+      />
+      <div className="flex items-center gap-2">
+        <input
+          name="isGoing"
+          type="checkbox"
+          checked={isGoing}
+          onChange={() => setIsGoing((prev) => !prev)}
+          className="p-1 text-black placeholder:text-light-grey"
+        />
+        <label htmlFor="isGoing">Going?</label>
+      </div>
+      {isGoing && (
+        <>
+          <div>
+            {/** TODO: mulitple address lines? */}
+            <label htmlFor="address">Address (for thank you card)</label>
+            <input
+              name="address"
+              className="w-full p-1 text-black placeholder:text-light-grey"
+            />
+            {fieldErrors?.address && <small>{fieldErrors.address}</small>}
+          </div>
+          <div>
+            <label htmlFor="dietaryRestrictions">
+              Dietary restrictions / preferences
+            </label>
+            <input
+              name="dietaryRestrictions"
+              className="w-full p-1 text-black placeholder:text-light-grey"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              name="plusOne"
+              type="checkbox"
+              checked={bringingPlusOne}
+              onChange={() => {
+                setBringingPlusOne((prev) => !prev);
+              }}
+              className="p-1 text-black placeholder:text-light-grey"
+            />
+            <label htmlFor="plusOne">Bringing plus one?</label>
+          </div>
+          {bringingPlusOne && (
+            <>
+              <div>
+                <label htmlFor="plusOneName">Plus one name</label>
+                <input
+                  name="plusOneName"
+                  className="w-full p-1 text-black placeholder:text-light-grey"
+                />
+              </div>
+              <div>
+                <label htmlFor="plusOneEmail">Plus one email</label>
+                <input
+                  name="plusOneEmail"
+                  type="email"
+                  className="w-full p-1 text-black placeholder:text-light-grey"
+                />
+              </div>
+            </>
+          )}
+          <div className="flex items-center gap-2">
+            <input
+              name="noGift"
+              type="checkbox"
+              className="p-1 text-black placeholder:text-light-grey"
+            />
+            <label htmlFor="noGift">
+              By responding, I hereby agree that I will NOT buy any gifts for
+              the couple. Your participation will be your gift. 💛
+            </label>
+          </div>
+        </>
+      )}
+      <ul>
+        <li>Make sure to read all details before </li>
+      </ul>
+      <button className="rounded-sm bg-blue p-2" type="submit">
+        Submit
+      </button>
     </Form>
   );
 };
